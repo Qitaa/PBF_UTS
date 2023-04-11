5ab94dc15e21243ffc1a77460811dd534b504228
Rizqita Siska L. R <71862974+Qitaa@users.noreply.github.com>
2023-04-11 11:03:02 +0700

    Awal

---
+++ uts/src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Sidebar() {
+  return (
+
+    <div className="fixed max-md:hidden">
+      <div className="flex flex-col items-center w-10 py-10 ml-7 space-y-7 shadow-xl rounded-xl">
+        <div className="space-y-30 rounded-md bg-white">
+          <ul>
+            <li className="p-7">
+              <Link to='/brands' >
+                <i className="text-3xl fa-solid fa-tag"></i>
+              </Link>
+            </li>
+            <li className="p-7">
+              <Link to='/categories' >
+                <i className="text-3xl fa-solid fa-list"></i>
+              </Link>
+            </li>
+            <li className="p-7">
+              <Link to='/banks' >
+                <i className="text-3xl fa-solid fa-building-columns"></i>
+              </Link>
+            </li>
+            <li className="p-7">
+              <Link to='/vehicles' >
+                <i className="text-3xl fa-solid fa-car"></i>
+              </Link>
+            </li>
+
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
